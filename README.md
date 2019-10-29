1c785a16bc980309a41d0ee959598ad5c6d1b6af
Sergey Mokhonko <sergiy35@gmail.com>
2019-10-29 22:30:57 +0200

    Done header and services block

---
+++ README.md
@@ -0,0 +1,1 @@
+# goldeb-web-design
---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
+module.exports = {
+    mode: "production",
+    entry: "./src/index.js",
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: 'main.js'
+    },
+    devServer: {
+        overlay: true
+    },
+    module: {
+        rules: [
+            {
+                test: /\.less$/,
+                exclude: /node_modules/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    "css-loader",
+                    "less-loader"
+                ]
+            },
+            // {
+            //     test: /\.css$/,
+            //     exclude: /node_modules/,
+            //     use: [
+            //         {
+            //             loader: MiniCssExtractPlugin.loader,
+            //         },
+            //         'css-loader'
+            //     ]
+            // }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html'
+        }),
+        new MiniCssExtractPlugin({
+            filename: "[name].css",
+            ignoreOrder: false
+        })
+    ]
+}
